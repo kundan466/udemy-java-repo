28e0738bb2433c727e528455ec31c9a3b9add6e4
kundanreddy <saikundan2002@gmail.com>
2023-12-23 19:58:54 +0530

    add Six.java

---
+++ springBootcamp/quiz2/Six.java
@@ -0,0 +1,25 @@
+package springBootcamp.quiz2;
+
+public class Six {
+    public static void main(String[] args) {
+        int x = 2;
+
+        switch (x) {
+
+            case 1:
+
+            case 2:
+
+            case 3:
+
+                System.out.println("Fly");
+
+                break;
+
+            case 4:
+
+                System.out.println("Swim");
+
+        }
+    }
+}
