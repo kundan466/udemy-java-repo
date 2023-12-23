2e78cc7eb7f410b0a61d5467ae614c8c7860e0f0
kundanreddy <saikundan2002@gmail.com>
2023-12-23 20:39:48 +0530

    add Ten.java

---
+++ springBootcamp/quiz3/Five.java
@@ -0,0 +1,25 @@
+package springBootcamp.quiz3;
+
+public class Five {
+    public static void main(String[] args) {
+        Message ob = new Message();
+
+        ob.printMessage(5);
+    }
+}
+
+class Message {
+
+    public void printMessage(int message) {
+
+        System.out.println("Message primitive: " + message);
+
+    }
+
+    public void printMessage(Integer message) {
+
+        System.out.println("Message object: " + message);
+
+    }
+
+}
---
+++ springBootcamp/quiz3/Ten.java
@@ -0,0 +1,15 @@
+package springBootcamp.quiz3;
+
+public class Ten {
+    public static void main(String[] args) {
+        String password[] = { "XY01", "XY02", "XY03", "XY04" };
+
+        String result = " ";
+
+        for (int i = password.length - 1; i >= 2; i--) {
+
+            result = result + password[i];
+        }
+        System.out.println(result);
+    }
+}
