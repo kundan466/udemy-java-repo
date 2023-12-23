c379c8af163c1577ae3d5fd70c65cfccfe1b8bec
kundanreddy <saikundan2002@gmail.com>
2023-12-23 18:54:44 +0530

    added First.java file

---
+++ java and spring/First.java
@@ -0,0 +1,19 @@
+// package javaandspring;
+
+// What will be the value of variables ‘a’ and ‘b’ after the execution of the given code?
+
+public class First {
+
+    public static void main(String[] args) {
+
+        int a, b;
+
+        a = b = 10;
+
+        System.out.println(a); // output is 10
+
+        System.out.println(b); // output is 10
+
+    }
+
+}
