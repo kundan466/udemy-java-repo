141288d026b5c8faf0c7c893feafc122bbbcfbae
kundanreddy <saikundan2002@gmail.com>
2023-12-23 19:45:49 +0530

    update in springBootcamp

---
+++ Second.java
@@ -0,0 +1,14 @@
+
+public class Second {
+    public static void main(String[] args) {
+
+        int a = 3;
+
+        int b = 6;
+
+        int result = (~a & b) | (a & ~b);
+
+        System.out.println(result);
+
+    }
+}
