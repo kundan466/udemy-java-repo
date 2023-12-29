6a378b33c673d88cefd5554b16213089a55e4b4f
Sai kundan Reddy <91199116+kundan466@users.noreply.github.com>
2023-12-29 10:47:54 +0530

    added quizapp

---
+++ quizapp/controller/RequestController.java
@@ -0,0 +1,5 @@
+package com.kundan.quizapp.controller;
+
+public @interface RequestController {
+
+}
---
+++ quizapp/service/QuizService.java
@@ -0,0 +1,66 @@
+package com.kundan.quizapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.kundan.quizapp.Dao.QuestionDao;
+import com.kundan.quizapp.Dao.QuizDao;
+import com.kundan.quizapp.model.Question;
+import com.kundan.quizapp.model.Quiz;
+import com.kundan.quizapp.model.Response;
+import com.kundan.quizapp.model.QuestionWrapper;
+
+@Service
+public class QuizService {
+    @Autowired
+    QuizDao quizDao;
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+
+        List<Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions(questions);
+        quizDao.save(quiz);
+
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questionsFromDB = quiz.getQuestions();
+
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+
+        for (Question q : questionsFromDB) {
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(),
+                    q.getOption3(), q.getOption4());
+            questionForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionForUser, HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for (Response response : responses) {
+            if (response.getResponse().equals(questions.get(i++).getRightAnswer())) {
+
+                right++;
+            }
+
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
