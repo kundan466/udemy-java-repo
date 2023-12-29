8694ee5a080d09ad33aa2abc71f5a142142c0ac3
Sai kundan Reddy <91199116+kundan466@users.noreply.github.com>
2023-12-29 10:53:45 +0530

    added quizapp

---
+++ springBootcamp/quizapp/Dao/QuizDao.java
@@ -0,0 +1,12 @@
+package com.kundan.quizapp.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.kundan.quizapp.model.Quiz;
+
+@Repository
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+
+}
