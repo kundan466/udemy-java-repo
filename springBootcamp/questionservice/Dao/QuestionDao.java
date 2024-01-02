f2ef6a388f6d9434ab1fd88ef961365b90bda998
Sai kundan Reddy <91199116+kundan466@users.noreply.github.com>
2024-01-02 11:30:25 +0530

    Add files via upload

---
+++ springBootcamp/questionservice/Dao/QuestionDao.java
@@ -0,0 +1,18 @@
+package com.kundan.questionservice.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.kundan.questionservice.model.Question;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT q.id FROM question q WHERE q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Integer> findRandomQuestionsByCategory(String category, int numQ);
+}
---
+++ springBootcamp/questionservice/service/QuestionService.java
@@ -0,0 +1,80 @@
+package com.kundan.questionservice.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.kundan.questionservice.Dao.QuestionDao;
+import com.kundan.questionservice.model.QuestionWrapper;
+import com.kundan.questionservice.model.Response;
+import com.kundan.questionservice.model.Question;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String categoryName, Integer numQuestions) {
+        List<Integer> questions = questionDao.findRandomQuestionsByCategory(categoryName, numQuestions);
+        return new ResponseEntity<>(questions, HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> wrappers = new ArrayList<>();
+        List<Question> questions = new ArrayList<>();
+
+        for (Integer id : questionIds) {
+            questions.add(questionDao.findById(id).get());
+        }
+        for (Question question : questions) {
+            QuestionWrapper qw = new QuestionWrapper(question.getId(), question.getQuestionTitle(),
+                    question.getOption1(), question.getOption2(), question.getOption3(), question.getOption4());
+        }
+
+        return new ResponseEntity<>(wrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+
+        int right = 0;
+
+        for (Response response : responses) {
+            Question question = questionDao.findById(response.getId()).get();
+            if (response.getResponse().equals(question.getRightAnswer())) {
+
+                right++;
+            }
+
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
